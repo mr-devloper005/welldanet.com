b021971af8af79af7a5f49d0299f07e011d33102
root <root@srv1489383.hstgr.cloud>
2026-05-21 06:46:04 +0000

    Add singular classified detail route alias

---
+++ src/app/classified/[slug]/page.tsx
@@ -0,0 +1,4 @@
+import ClassifiedDetailPage, { generateMetadata, generateStaticParams, revalidate } from "../../classifieds/[slug]/page";
+
+export { generateMetadata, generateStaticParams, revalidate };
+export default ClassifiedDetailPage;
